d19f7535e43e060c0f380e378f75dc68f98ab435
David González Martínez <me@davidgonmar.me>
2025-05-12 23:40:28 +0200

    feat: add app

--- simple_app.py
+++
@@ -1,8 +0,0 @@
-from nicegui import ui
-
-def main():
-    with ui.card():
-        ui.label('Aplicación de prueba de NiceGUI').classes('text-2xl')
-        ui.button('Haz clic aquí', on_click=lambda: ui.notify('¡Funciona!'))
-
-ui.run(port=8080) 
